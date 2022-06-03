082d7f3b49f00005ed48e3cd748e8a657e508f30
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-03 13:40:10 -0700

    add index and create routes

---
+++ PierresSweets/Controllers/TreatsController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PierresSweets.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierresSweets.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly PierresSweetsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public TreatsController(UserManager<ApplicationUser> userManager, PierresSweetsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Index ()
+    {
+      List<Treat> model = _db.Treats.ToList();
+      return View(model);
+    }
+
+    [Authorize]
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Treat treat)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
