0ac98a0bbe69c6f0d9b789d123600417203279c6
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-03 10:53:22 -0700

    add database and viewbags for flavors/treats to display on splash page

---
+++ PierresSweets/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PierresSweets.Models;
+
+namespace PierresSweets.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      private readonly PierresSweetsContext _db;
+      public HomeController(PierresSweetsContext db)
+      {
+        _db = db;
+      }
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        ViewBag.Flavors = _db.Flavors.ToList();
+        ViewBag.Treats = _db.Treats.ToList();
+        return View();
+      }
+
+    }
+}
