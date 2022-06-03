8151ba35874a6aa5ef267c864f6269651d1bea44
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-03 10:32:48 -0700

    update Flavor and Treat models to match project input

---
+++ PierresSweets/Models/Flavor.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace PierresSweets.Models
+{
+  public class Flavor
+  {
+    public Flavor() 
+    {
+      this.TreatFlavor = new HashSet<TreatFlavor>();
+    }
+
+    public int FlavorId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<TreatFlavor> TreatFlavor { get; set; }
+  }
+}
---
+++ PierresSweets/Models/Treat.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+  public class Treat
+  {
+    public Treat() 
+    {
+      this.TreatFlavor = new HashSet<TreatFlavor>();
+    }
+
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<TreatFlavor> TreatFlavor { get; }
+  }
+}
